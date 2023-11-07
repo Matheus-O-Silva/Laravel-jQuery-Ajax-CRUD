e098e8ef85300a4507b788e50a0154e0d061ed17
Matheus Oliveira <matheus.oliveira110101@gmail.com>
2023-11-07 00:56:19 -0300

    disciplines table

--- database/migrations/2022_03_19_120906_create_disciplines_table.php
+++ database/migrations/2022_03_19_120906_create_disciplines_table.php
@@ -13,12 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('disciplines', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email');
-            $table->string('phone');
-            $table->string('course');
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('students');
+        Schema::dropIfExists('disciplines');
     }
 };
